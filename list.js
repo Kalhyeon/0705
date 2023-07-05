b55ec2d3b107cfceced6ac22067870c60d3a92c7
Kalhyeon <grow0728@gmail.com>
2023-07-05 17:34:21 +0900

    Added some JS

--- list.js
+++ list.js
@@ -28,3 +28,61 @@ async function fetch(pageno=1, pagesize=10) {
     return null;
   }
 }
+
+function printContacts(contacts) {
+  const $parent = $('#tbody');
+  for(c of contacts) {
+    const html = `
+      <tr>
+        <td>${c.no}</td>
+        <td><a href='read.html?no=${c.no}'>${c.name}</a></td>
+        <td>${c.tel}</td>
+        <td>${c.address}</td>
+      </tr>
+    `;
+    $parent.append(html);
+  }
+}
+
+// pagination 에 필요한 prev, start, end, next, pageno 를 리턴하는 함수
+// getPagination(result) => result 에서 pageno, pagesize, totalcount 를 꺼내는 문법
+// 구조 분해 할당
+function getPagination({pageno, pagesize, totalcount, blockSize=5}) {
+  // countOfPage : 페이지의 개수 계산
+  const countOfPage = Math.ceil(totalcount/pagesize);
+  // prev, start, end, next 를 계산한 다음 목록의 끝에 도달한 경우, end/next 를 변경
+  const prev = Math.floor((pageno-1)/blockSize)*blockSize;
+  const start = prev+1;
+  let end = prev + blockSize;
+  let next = end + 1;
+  if(end>=countOfPage) {
+    end = countOfPage;
+    next = 0;
+  }
+  // 구조 분해 할당 : 객체를 변수로 분해, 변수를 모아 객체를 리턴
+  return {prev, start, end, next, pageno};
+  // = return {prev:prev, start:start, end:end, next:next, pageno:pageno};
+}
+
+function printPagination({prev, start, end, next, pageno}) {
+  const $parent = $('#pagination');
+  if(prev>0) {
+    const html =
+    `<li class="page-item"><a href="list.html?pageno=${prev}" class="page-link">이전으로</a></li>`;
+    $parent.append(html);
+  }
+  for(let i=start; i<=end; i++) {
+    let className = 'page-item';
+    if(i===pageno) {
+      className = 'page-item active'
+    }
+    const html =
+    `<li class="${className}"><a href="list.html?pageno=${i}" class="page-link">${i}</a></li>`;
+    $parent.append(html);
+  }
+  if(next>0) {
+    const html =
+    `<li class="page-item"><a href="list.html?pageno=${next}" class="page-link">다음으로</a></li>`;
+    $parent.append(html);
+  }
+}
